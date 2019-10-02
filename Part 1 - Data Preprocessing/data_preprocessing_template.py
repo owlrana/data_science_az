59166a2239e4ddb67718ba15100dc46bdde865f6
Rahul Rana <raulrenne@outlook.com>
2019-10-03 01:15:19 +0530

    Update data_preprocessing_template.py

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -1,5 +1,5 @@
-# Data Preprocessing Template
-
+# Data Preprocessing Template (I use this template to pre-process my dataset for modelling everytime I create a new model)
+# You can use this for your own projects or contribute if you find something relevant missing
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,11 +10,11 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
+# Splitting the dataset into the Training set and Test set (Needed to prevent Overfitting)
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+# Feature Scaling (we may need it at certain time)
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
