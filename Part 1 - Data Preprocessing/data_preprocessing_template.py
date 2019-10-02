ee7435ca483c16fc232de927933109e0e9e12f97
Rahul Rana <rahulrana@null.net>
2019-10-03 01:24:21 +0530

    Update data_preprocessing_template.py
    Better Documentation

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# Importing the dataset to your machine
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
