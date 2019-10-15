358b2a2b0bd69fae3e048d21a21b3d699c106f7e
Ishaan1342 <55406267+Ishaan1342@users.noreply.github.com>
2019-10-15 18:17:54 +0530

    Update missing_data.py

--- Part 1 - Data Preprocessing/missing_data.py
+++ Part 1 - Data Preprocessing/missing_data.py
@@ -14,4 +14,5 @@ y = dataset.iloc[:, 3].values
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+X[:, 1:3] = imputer.tra nsform(X[:, 1:3])
+# will be creating sequence parts in java
